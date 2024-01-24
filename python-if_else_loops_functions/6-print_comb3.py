93a50dae1f8111d534b418ed6c7bbfec21604a9d
root <root@LAPTOP-2LV4F0PM>
2024-01-24 12:26:10 +0100

    comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i % 10 != (i // 10) % 10 and i < 99:
-        print(f"{i}, ", end='')
-    else:
-        print(i, end=' ')
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
