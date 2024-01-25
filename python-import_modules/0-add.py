35ad90441e9cca265489ac4053149d568a7ca0b9
root <root@LAPTOP-2LV4F0PM>
2024-01-25 12:51:27 +0100

    add

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Assign the value 1 to the variable a
+a = 1
+# Assign the value 2 to the variable b
+b = 2
+# Import the add function from the file add_0.py
+from add_0 import add
+
+# Use the variables a and b as arguments for the add function
+print(f"{a} + {b} = {add(a, b)}")
+
