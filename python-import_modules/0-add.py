ebe8124fbaf5f8416ab52ced9ba79c39b991cb55
root <root@LAPTOP-2LV4F0PM>
2024-01-25 14:02:59 +0100

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    a = 1
-    b = 2
 # Import the add function from the file add_0.py
     from add_0 import add
+    a = 1
+    b = 2
 
 # Use the variables a and b as arguments for the add function
     print(f"{a} + {b} = {add(a, b)}")
-
