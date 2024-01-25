4dd3370e8f105a73044249293b0bdd41d49f1af5
root <root@LAPTOP-2LV4F0PM>
2024-01-25 15:34:21 +0100

    Fix task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-
 # Use the variables a and b as arguments for the add function
     print("{} + {} = {}".format(a, b, add(a, b)))
