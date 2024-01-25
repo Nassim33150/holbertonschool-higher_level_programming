b2f1ce486d49b9ab3263f897fcbcbf6433dc640d
root <root@LAPTOP-2LV4F0PM>
2024-01-25 15:50:57 +0100

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-num_args = len(argv) - 1
+    num_args = len(argv) - 1
 
 if num_args == 0:
     print(f"{0} {'arguments'}{'.'}")
