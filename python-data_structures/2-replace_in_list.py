8d3abd0272087dd210ef5dd07382976f27323707
root <root@LAPTOP-2LV4F0PM>
2024-01-31 11:03:26 +0100

    replace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     for i in range(len(my_list)):
-        if my_list[i] == element:
-            my_list[idx] = idx
+        if my_list[i] == idx:
+            my_list[idx] = element
             return my_list
