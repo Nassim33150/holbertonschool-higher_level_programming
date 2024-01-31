949c5465aaa4b37829c66dfd044291b57741d004
root <root@LAPTOP-2LV4F0PM>
2024-01-31 14:25:27 +0100

    print_sorted

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    myKeys = list(a_dictionary.keys())
-    myKeys.sort()
-    sorted_dict = {i: a_dictionary[i] for i in myKeys}
-    print (sorted_dict)
+    my_keys = list(a_dictionary.keys())
+    my_keys.sort()
+    for key in my_keys:
+        print(f"{key}: {a_dictionary[key]}")
