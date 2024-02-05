779a00db3c4c00e2b3a1d525404a72ea9e063e95
root <root@LAPTOP-2LV4F0PM>
2024-02-05 14:48:04 +0100

    square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size = 0):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size but be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else :
+                self.__size = size
+        except (TypeError, ValueError) as e:
+            print(e)
+    def area(self):
+        return self.__size ** 2
