a002e527d36a2e2b9d58b536dfa40d9be2ffd683
root <root@LAPTOP-2LV4F0PM>
2024-02-09 16:34:37 +0100

    text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,26 @@
 def text_indentation(text):
+    """
+    Add two new lines after each occurrence of '.', '?',
+    and ':' characters in the text.
+
+    Args:
+        text (str): The text to be formatted.
+
+    Raises:
+        TypeError: If the input is not a string.
+
+    Returns:
+        None
+    """
+    """Check if the input is a string"""
     if not isinstance(text, (str)):
         raise TypeError("text must be a string")
+        """Iterate over each character in the text"""
     for char in text:
+        """If the character is '.', '?', or ':',
+        print it followed by two new lines"""
         if char in ['.', '?', ':']:
             print(char + "\n\n", end="")
+        """Otherwise, print the character without any changes"""
         else:
             print(char, end="")
