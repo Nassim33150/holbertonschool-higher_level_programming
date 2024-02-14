46bacb5776044d1ca6f667c062ce1469fd67c415
root <root@LAPTOP-2LV4F0PM>
2024-02-14 10:49:02 +0100

    base geometry with exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" create a class """
+
+
+class BaseGeometry:
+    """ function that raise exception """
+    def area(self):
+        raise Exception("area() is not implemented")
