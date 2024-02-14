08a11bac1fd6651a66bb4cb8741dc8cb558c3dc8
root <root@LAPTOP-2LV4F0PM>
2024-02-14 12:54:47 +0100

    class inherit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 """ creates a class that herites form class list """
 
 
 class MyList(list):
+    """MyList class - Inherits from list"""
     def print_sorted(self):
         """ sort list in ascending """
         n = len(self)
