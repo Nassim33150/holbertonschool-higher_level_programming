4d1b2b56f642ca62a4eade5cf4f08c94e14e9a1d
root <root@LAPTOP-2LV4F0PM>
2024-02-14 14:47:37 +0100

    sort list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,9 +6,4 @@ class MyList(list):
     """MyList class - Inherits from list"""
     def print_sorted(self):
         """ sort list in ascending """
-        n = len(self)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if self[i] > self[j]:
-                    self[i], self[j] = self[j], self[i]
-        return self
+        print(sorted(self))
