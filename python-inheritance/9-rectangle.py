90bf24f124af4f39e93bb5e933c30d79fb1026cb
root <root@LAPTOP-2LV4F0PM>
2024-02-14 15:20:27 +0100

    return area and str

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+""" create subclass from BaseGeometry """
+
+
+class Rectangle(BaseGeometry):
+    """initializes attributes : width and height"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
