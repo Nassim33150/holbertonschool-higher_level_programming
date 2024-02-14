53f3e095a83c4cbf630d2cc32acbdf89d9194063
root <root@LAPTOP-2LV4F0PM>
2024-02-14 09:13:04 +0100

    create class and tests

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+""" creates a class that herites form class list """
+
+
+class MyList(list):
+    def print_sorted(self):
+        """ sort list in ascending """
+        n = len(self)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if self[i] > self[j]:
+                    self[i], self[j] = self[j], self[i]
+        return self
