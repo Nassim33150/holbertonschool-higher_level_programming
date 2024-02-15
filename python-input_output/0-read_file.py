a14136e0d894379f052afce7a0a1041f5695e665
root <root@LAPTOP-2LV4F0PM>
2024-02-15 11:43:27 +0100

    read a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as file:
         content = file.read()
         print(content, end="")
