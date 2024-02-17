03c34bcb9a62a1f710cef0fa15dfa6b86ffa94e5
root <root@LAPTOP-2LV4F0PM>
2024-02-17 18:17:30 +0100

    append to file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding="utf-8") as file:
+    """Appends a string to the end of a UTF8 text file."""
+    with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
