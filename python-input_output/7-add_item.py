913b31baa1795d6756dfc95c8d9412f122e47035
root <root@LAPTOP-2LV4F0PM>
2024-02-17 13:00:53 +0100

    add item and load

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
 """
 
 import sys
+import os.path
 
 list = sys.argv[1:]
 
@@ -12,7 +13,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 """ load the object JSON file """
-loaded = load_from_json_file('add_item.json')
+loaded = []
+if os.path.exists('./add_item.json'):
+    loaded = load_from_json_file('add_item.json')
 
 """ save the list python in text file JSON """
 save_to_json_file(loaded + list, 'add_item.json')
