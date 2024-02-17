26fa7268eeffebc2cac3ec2862646addeae1ee10
root <root@LAPTOP-2LV4F0PM>
2024-02-17 12:51:50 +0100

    add item and load

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,10 +5,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 list = sys.argv[1:]
 
-
-""" save the list python in text file JSON """
-save_to_json_file(list, 'add_item.json')
-
-
 """ load the object JSON file """
 loaded = load_from_json_file('add_item.json')
+
+""" save the list python in text file JSON """
+save_to_json_file(loaded + list, 'add_item.json')
