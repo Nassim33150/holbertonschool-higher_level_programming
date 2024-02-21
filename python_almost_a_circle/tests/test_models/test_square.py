0ee727c180db9ee69a07aa3d62b5057f723fdbee
root <root@LAPTOP-2LV4F0PM>
2024-02-21 11:33:48 +0100

    test square

---
+++ python_almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Module for test Rectangle class """
+from unittest import TestCase
+from models.rectangle import Rectangle
+from models.base import Base
+from models.Square import Square
