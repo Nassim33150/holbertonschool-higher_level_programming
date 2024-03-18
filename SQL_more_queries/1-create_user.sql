deaca60cd931c6ec53d0d86b2c27d849b7879a8d
root <root@LAPTOP-2LV4F0PM>
2024-03-18 10:55:21 +0100

    create user with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+SET @user_exists = (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost');
+
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+END IF;
