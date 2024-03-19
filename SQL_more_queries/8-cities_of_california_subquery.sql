32a8f2737d4cd8bdbec1a92ed779d60a0286fabf
root <root@LAPTOP-2LV4F0PM>
2024-03-19 13:47:20 +0100

    add description

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- Select all columns from the 'cities' table in the 'hbtn_0d_usa' database
 -- Order the results by the 'id' column in ascending order
 SELECT * FROM hbtn_0d_usa.cities
+WHERE state_id = 1
 ORDER BY cities.id ASC;
