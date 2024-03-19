f0d0004573bc28a474dc48e80b350430cd465e2f
root <root@LAPTOP-2LV4F0PM>
2024-03-19 11:28:10 +0100

    add description

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,4 @@
+-- Displays the privileges granted to the user 'user_0d_1' when connecting from localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Displays the privileges granted to the user 'user_0d_2' when connecting from localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
