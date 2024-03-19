aa5f2dc4d94a66165659b67afd8452f882e84ab9
root <root@LAPTOP-2LV4F0PM>
2024-03-19 14:14:38 +0100

    delete *

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,10 @@
 -- Select id and name  from the 'cities' table in the 'hbtn_0d_usa' database
 -- Order the results by the 'id' column in ascending order
-SELECT id, name FROM hbtn_0d_usa.cities
-WHERE state_id = 1
-ORDER BY cities.id ASC;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id;
