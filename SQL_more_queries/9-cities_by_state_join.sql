d852c9da1c8a4ce75eb0dfaa0c69d7c05f9959cc
root <root@LAPTOP-2LV4F0PM>
2024-03-19 15:08:08 +0100

    cities by state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Select the id and name columns from the 'cities' table, and the name column from the 'states' table
 -- Join the 'cities' table with the 'states' table based on the 'id' and 'state_id' columns, respectively
 -- Order the results by the 'id' column of the 'cities' table in ascending order
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.states
-INNER JOIN cities ON states.id = cities.state_id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id
