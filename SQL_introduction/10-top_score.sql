f03122054c16f246a9e4633b81ba332db8ea4e16
root <root@LAPTOP-2LV4F0PM>
2024-03-19 15:48:53 +0100

    removes db

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Retrieve the 'score' and 'name' columns from the 'second_table' table in the 'hbtn_0c_0' database
 -- Order the results by 'score' in descending order
-SELECT score, name FROM hbtn_0c_0.second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC;
