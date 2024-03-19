ce801e8112eb8f5b31e4a6cb0513762018009dfb
root <root@LAPTOP-2LV4F0PM>
2024-03-19 15:43:02 +0100

    add database

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
 -- Count the number of records where the 'id' column equals 89 in the 'first_table' table
-SELECT COUNT(id = 89) FROM hbtn_0c_0.first_table;
+SELECT COUNT(*) 
+FROM hbtn_0c_0.first_table 
+WHERE id = 89;
