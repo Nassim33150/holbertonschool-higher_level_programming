9091f3028cb5f07e45a69e4eef80956b6617acca
root <root@LAPTOP-2LV4F0PM>
2024-03-20 10:20:38 +0100

    count show by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Select the genre of TV show and count the number of shows linked to each genre
+SELECT tv_show_genres.name AS 'genre',
+    COUNT(*) AS 'number_of_shows'
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id= tv_show_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
