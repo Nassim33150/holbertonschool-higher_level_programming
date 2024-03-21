66062d932421d2ab382973db12db4e0a0fb25275
root <root@LAPTOP-2LV4F0PM>
2024-03-21 10:08:09 +0100

    select states ORM

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,22 +2,23 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
     database = MySQLdb.connect(
-        host = "localhost",
-        user = sys.argv[1],
-        password = sys.argv[2],
-        database = sys.argv[3],
-        port = 3306
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
     )
 
-cur = database.cursor()
+    cur = database.cursor()
 
-req = 'SELECT * FROM states'
-cur.execute(req)
+    req = 'SELECT * FROM states'
+    cur.execute(req)
 
-citieslist = cur.fetchall()
+    citieslist = cur.fetchall()
 
-for city in citieslist:
-    print(city)
-cur.close()
-database.close()
+    for city in citieslist:
+        print(city)
+    cur.close()
+    database.close()
