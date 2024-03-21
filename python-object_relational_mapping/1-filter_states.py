d4b8a2ca792c9089d4378a9b167a4c12d3085696
root <root@LAPTOP-2LV4F0PM>
2024-03-21 10:21:25 +0100

    filter states with ORM

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+
+    cur = database.cursor()
+
+    req = 'SELECT * FROM states'
+    cur.execute(req)
+
+    citieslist = cur.fetchall()
+
+    for city in citieslist:
+        if city[1].startswith('N'):
+            print(city)
+    cur.close()
+    database.close()
