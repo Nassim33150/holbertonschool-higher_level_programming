7b7277f9635c4e5eb5625f838907825b22b196da
root <root@LAPTOP-2LV4F0PM>
2024-03-21 10:31:20 +0100

    filter states with ORM

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     cur = database.cursor()
 
-    req = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id"
+    req = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
     cur.execute(req)
 
     citieslist = cur.fetchall()
