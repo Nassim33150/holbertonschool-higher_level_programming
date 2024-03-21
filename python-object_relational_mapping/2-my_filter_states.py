f04e581273d9d49831f3be0b1f476143987bb9c5
root <root@LAPTOP-2LV4F0PM>
2024-03-21 10:42:43 +0100

    filter spaces

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+import MySQLdb
+import sys
+""" takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name
+matches the argument. """
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+
+    cur = database.cursor()
+
+    state = sys.argv[4]
+
+    req = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+    cur.execute(req, (state,))
+
+    citieslist = cur.fetchall()
+
+    for city in citieslist:
+        print(city)
+    cur.close()
+    database.close()
