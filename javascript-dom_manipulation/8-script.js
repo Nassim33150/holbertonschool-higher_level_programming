c2eed9212c1735de8fe63f87e40f8d0789f12894
root <root@LAPTOP-2LV4F0PM>
2024-05-24 09:35:56 +0200

    task 8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const helloDiv = document.querySelector('#hello');
+
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloDiv.textContent = data.hello;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
